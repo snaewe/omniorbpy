afc3bef1db1b14e7e1882189e3e2993679d102c9
dpg1 <dpg1>
1999-07-29 14:20:44 +0000

    Oneway support. Exception handling modified.

--- modules/omni30/pyCallDescriptor.cc
+++ modules/omni30/pyCallDescriptor.cc
@@ -5,6 +5,9 @@
 // $Id$
 
 // $Log$
+// Revision 1.7  1999/07/29 14:20:44  dpg1
+// Oneway support. Exception handling modified.
+//
 // Revision 1.6  1999/07/19 16:49:32  dpg1
 // Recursive marshalling functions moved out of proxy call class into
 // omniPy module so the can be shared with the server-side.
@@ -58,6 +61,10 @@ omniPy::Py_OmniProxyCallDesc::unmarshalReturnedValues(GIOP_C& giop_client)
   PyEval_RestoreThread(tstate_);
   tstate_ = 0;
 
+  assert(out_l_ >= 0);
+  // out_l_ == -1 if it's a oneway operation, but we should never
+  // reach here if that is the case.
+
   if (out_l_ == 0) {
     Py_INCREF(Py_None);
     result_ = Py_None;
@@ -87,8 +94,26 @@ omniPy::Py_OmniProxyCallDesc::userException(GIOP_C&     giop_client,
 
   PyObject* d_o = PyDict_GetItemString(exc_d_, repoId);
 
-  if (d_o) { // exc class, name, descriptor, name, descriptor, ...
-    PyObject* exc_i = unmarshalPyObject(giop_client, d_o);
+  if (d_o) { // class, repoId, exc name, name, descriptor, ...
+
+    // Can't use normal exception unmarshalling code, since the repoId
+    // has already been unmarshalled.
+
+    PyObject* excclass = PyTuple_GET_ITEM(d_o, 1);
+    assert(PyClass_Check(excclass));
+
+    int       cnt      = (PyTuple_GET_SIZE(d_o) - 4) / 2;
+    PyObject* exctuple = PyTuple_New(cnt);
+
+    int i, j;
+    for (i=0, j=5; i < cnt; i++, j+=2) {
+      PyTuple_SET_ITEM(exctuple, i,
+		       unmarshalPyObject(giop_client,
+					 PyTuple_GET_ITEM(d_o, j)));
+    }
+    PyObject* exc_i = PyEval_CallObject(excclass, exctuple);
+    Py_DECREF(exctuple);
+
     PyErr_SetObject(PyTuple_GET_ITEM(d_o, 1), exc_i);
     throw CORBA::UserException();
   }
@@ -96,4 +121,27 @@ omniPy::Py_OmniProxyCallDesc::userException(GIOP_C&     giop_client,
     giop_client.RequestCompleted(1);
     throw CORBA::MARSHAL(0, CORBA::COMPLETED_MAYBE);
   }
+}
+
+
+CORBA::ULong
+omniPy::Py_OmniOWProxyCallDesc::alignedSize(CORBA::ULong msgsize)
+{
+  for (int i=0; i < in_l_; i++)
+    msgsize = omniPy::alignedSize(msgsize,
+				  PyTuple_GET_ITEM(in_d_,i),
+				  PyTuple_GET_ITEM(args_,i));
+  return msgsize;
+}
+
+
+void
+omniPy::Py_OmniOWProxyCallDesc::marshalArguments(GIOP_C& giop_client)
+{
+  for (int i=0; i < in_l_; i++)
+    omniPy::marshalPyObject(giop_client,
+			    PyTuple_GET_ITEM(in_d_,i),
+			    PyTuple_GET_ITEM(args_,i));
+
+  if (!tstate_) tstate_ = PyEval_SaveThread();
 }
