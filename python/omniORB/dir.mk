cba9cdd5617d1d112c4bc975de3eff840c277de3
dpg1 <dpg1>
1999-09-24 11:32:20 +0000

    Added PortableServer.py to exports.

--- python/omniORB/dir.mk
+++ python/omniORB/dir.mk
@@ -6,5 +6,8 @@ export:: __init__.py
 export:: CORBA.py
 	@(file="CORBA.py"; dir="$(PYLIBDIR)"; $(ExportFileToDir))
 
+export:: PortableServer.py
+	@(file="PortableServer.py"; dir="$(PYLIBDIR)"; $(ExportFileToDir))
+
 export:: tcInternal.py
 	@(file="tcInternal.py"; dir="$(PYLIBDIR)"; $(ExportFileToDir))
