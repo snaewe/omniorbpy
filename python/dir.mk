851f480f510b0120463d22a55849bc30f5236e49
dpg1 <dpg1>
1999-09-27 16:41:02 +0000

    Stubs now live at top-level directory rather than omnistub/ directory.

--- python/dir.mk
+++ python/dir.mk
@@ -1,10 +1,13 @@
 PYBINDIR = $(EXPORT_TREE)/bin/scripts
 PYLIBDIR = $(EXPORT_TREE)/lib/python
 
-SUBDIRS = omniORB lib_omnistub CosNaming POA_CosNaming
+SUBDIRS = omniORB CosNaming POA_CosNaming
 
 all::
 	@$(MakeSubdirs)
 
 export::
 	@$(MakeSubdirs)
+
+export:: Naming_idl.py
+	@(file="Naming_idl.py"; dir="$(PYLIBDIR)"; $(ExportFileToDir))
