09261407af6872ccf319e2d5a690b59f08d9d1bc
Duncan Grisby <dgrisby@apasphere.com>
2007-09-18 20:04:14 +0000

    Set _parent_id on enums generated from TypeCodes.

--- modules/pyTypeCode.cc
+++ modules/pyTypeCode.cc
@@ -29,6 +29,9 @@
 
 // $Id$
 // $Log$
+// Revision 1.1.4.9  2007/09/18 20:04:14  dgrisby
+// Set _parent_id on enums generated from TypeCodes.
+//
 // Revision 1.1.4.8  2006/05/24 18:33:42  dgrisby
 // Off by one error in abstract interface typecode marshalling.
 //
@@ -1052,6 +1055,7 @@ r_unmarshalTypeCode(cdrStream& stream, OffsetDescriptorMap& odm)
 	  Py_DECREF(mname);
 
 	  OMNIORB_ASSERT(t_o);
+          PyObject_SetAttrString(t_o, (char*)"_parent_id", repoId);
 	  PyTuple_SET_ITEM(mems, i, t_o);
 	}
       }
